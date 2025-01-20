969ba4760895606a1a17af67ed16d1a7e98d5850
opilane <opilane@HP-CZC2349HT2>
2025-01-20 11:27:17 +0200

    print, int funktsioonid 20.01.25

---
+++ Pyhikonstruktsioonid-master/Pyhikonstruktsioonid.py
@@ -0,0 +1,40 @@
+print("Hello World!")
+#kommentaar
+# andmetüübid täisarv - int, ujukomaarv - float, sõne - str, tõevärtused - bool
+#astendamine **
+print()
+a=5
+aste=2
+tulemus=a**aste
+print(a,"**",aste,"=",tulemus)
+#korrutamine *
+arv1=int(input("Esimene korrutaja: "))
+arv2=int(input("Teine korrutaja: "))
+tulemus=arv1*arv2
+print(arv1,"*",arv2,"=",tulemus)
+#jagamine /
+arv1=int(input("Esimene arv: "))
+arv2=int(input("Jagaja: "))
+tulemus=arv1/arv2
+print(arv1,"/",arv2,"=",tulemus)
+print(type(tulemus))
+#jagamisjääk %
+arv1=int(input("Esimene arv: "))
+arv2=int(input("Teine arv: "))
+tulemus=arv1%arv2
+print(arv1,"%",arv2,"=",tulemus)
+#liitmine +
+arv1=int(input("Esimene arv: "))
+arv2=int(input("Teine arv: "))
+tulemus=arv1+arv2
+print(arv1,"+",arv2,"=",tulemus)
+#lahutaminé -
+arv1=int(input("Esimene arv: "))
+arv2=int(input("Teine arv: "))
+tulemus=arv1+arv2
+print(arv1,"-",arv2,"=",tulemus)
+#lahutaminé ˇ//
+arv1=int(input("Esimene arv: "))
+arv2=int(input("Teine arv: "))
+tulemus=arv1+arv2
+print(arv1,"//",arv2,"=",tulemus)
